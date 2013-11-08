c0b5b9ef3c419ac5bae7116eb553f00e2c3f7d14
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2013-11-08 10:15:13 +0100

    Removed the test files from the handlebars plugin.

--- test/org/thymeleaf/extras/idea/format/HbFormattingModelBuilderTest.java
+++
@@ -1,7 +0,0 @@
-package org.thymeleaf.extras.idea.format;
-
-/**
- * Marker interface to denote tests related to {@link org.thymeleaf.extras.idea.format.HbFormattingModelBuilder}
- */
-public interface HbFormattingModelBuilderTest {
-}
