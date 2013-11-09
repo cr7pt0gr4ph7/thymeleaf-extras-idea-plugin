6634b1ac7830724e1f1d48391635d6acdec5be34
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2013-11-09 12:27:30 +0100

    Moved and renamed DialectDomFileDescription to DialectsDomFileDescription in another package.

--- src/org/thymeleaf/extras/idea/dialect/DialectsDomFileDescription.java
+++ src/org/thymeleaf/extras/idea/dialect/DialectsDomFileDescription.java
@@ -1,10 +1,10 @@
-package org.thymeleaf.extras.idea.dialect.xml.dom;
+package org.thymeleaf.extras.idea.dialect;
 
 import com.intellij.util.xml.DomFileDescription;
 import org.thymeleaf.extras.idea.dialect.xml.Dialect;
 
-public class DialectDomFileDescription extends DomFileDescription<Dialect> {
-    public DialectDomFileDescription() {
+public class DialectsDomFileDescription extends DomFileDescription<Dialect> {
+    public DialectsDomFileDescription() {
         super(Dialect.class, "dialect", "http://www.thymeleaf.org/extras/dialect");
     }
 }
