4626b071e09337c8484fef290d13929b5035f629
cr7pt0gr4ph7 <cr7pt0gr4ph7@gmail.com>
2013-11-24 18:43:00 +0100

    Added missing @Override annotations and refactored the dialect descriptor index.

--- src/org/thymeleaf/extras/idea/dialect/discovery/DialectDescriptorIndex.java
+++ src/org/thymeleaf/extras/idea/dialect/discovery/DialectDescriptorIndex.java
@@ -3,14 +3,12 @@ package org.thymeleaf.extras.idea.dialect.discovery;
 import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.packaging.artifacts.Artifact;
-import com.intellij.packaging.impl.artifacts.ArtifactBySourceFileFinder;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.NullableFunction;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.DataIndexer;
 import com.intellij.util.indexing.FileBasedIndex;
 import com.intellij.util.indexing.FileContent;
@@ -20,13 +18,13 @@ import com.intellij.util.text.CharArrayUtil;
 import com.intellij.util.xml.NanoXmlUtil;
 import com.intellij.xml.index.IndexedRelevantResource;
 import com.intellij.xml.index.XmlIndex;
+import com.intellij.xml.util.XmlUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -38,9 +36,12 @@ public class DialectDescriptorIndex extends XmlIndex<DialectDescriptorIndex.Dial
     public static final ID<String, DialectInfo> NAME = ID.create("thymeleafDialectUris");
     private static final int INDEX_VERSION = 1;
 
+    // TODO Rename to getResourcesByNamespace
     public static List<IndexedRelevantResource<String, DialectInfo>> getDialectDescriptorFiles(String namespace, @NotNull Module module, @Nullable PsiFile context) {
         // TODO Should we use DumbService.isDumb() here?
-        if(DumbService.isDumb(module.getProject())) return Collections.emptyList();
+        if (DumbService.isDumb(module.getProject()) || (context != null && XmlUtil.isStubBuilding(context))) {
+            return Collections.emptyList();
+        }
 
         final GlobalSearchScope scope = computeAdditionalScope(module, context);
         final List<IndexedRelevantResource<String, DialectInfo>> resources = IndexedRelevantResource.getResources(NAME, namespace, module, module.getProject(), scope);
@@ -52,43 +53,44 @@ public class DialectDescriptorIndex extends XmlIndex<DialectDescriptorIndex.Dial
 
     @Nullable
     private static GlobalSearchScope computeAdditionalScope(Module module, @Nullable PsiFile context) {
-        if (context == null) {
-            return null;
-        }
-        final VirtualFile file = context.getVirtualFile();
-        if (file == null) {
-            return null;
-        }
-        final ArtifactBySourceFileFinder finder = ArtifactBySourceFileFinder.getInstance(module.getProject());
-        final Collection<? extends Artifact> artifacts = finder.findArtifacts(file);
-        if (artifacts.isEmpty()) {
-            return null;
-        }
-        return new GlobalSearchScope() {
+        return createFilterForProject(module.getProject());
+    }
+
+    private static GlobalSearchScope createFilterForProject(final Project project) {
+        final GlobalSearchScope projectScope = GlobalSearchScope.allScope(project);
+        return new GlobalSearchScope(project) {
+            @Override
             public boolean contains(VirtualFile file) {
-                Collection<? extends Artifact> a = finder.findArtifacts(file);
-                return ContainerUtil.intersects(artifacts, a);
+                final VirtualFile parent = file.getParent();
+                return parent != null && projectScope.contains(file);
             }
 
+            @Override
             public int compare(VirtualFile file1, VirtualFile file2) {
-                return 0;
+                return projectScope.compare(file1, file2);
             }
 
+            @Override
             public boolean isSearchInModuleContent(@NotNull Module aModule) {
                 return true;
             }
 
+            @Override
             public boolean isSearchInLibraries() {
-                return false;
+                return true;
             }
         };
     }
 
+    // TODO Rename to getAllResources
     public static List<IndexedRelevantResource<String, DialectInfo>> getDialectDescriptorFiles(final Module module, final PsiFile context) {
         // TODO Should we use DumbService.isDumb() here?
-        if(DumbService.isDumb(module.getProject())) return Collections.emptyList();
+        if (DumbService.isDumb(module.getProject()) || (context != null && XmlUtil.isStubBuilding(context))) {
+            return Collections.emptyList();
+        }
 
         return IndexedRelevantResource.getAllResources(NAME, module, module.getProject(), new NullableFunction<List<IndexedRelevantResource<String, DialectDescriptorIndex.DialectInfo>>, IndexedRelevantResource<String, DialectInfo>>() {
+            @Override
             public IndexedRelevantResource<String, DialectDescriptorIndex.DialectInfo> fun
                     (final List<IndexedRelevantResource<String, DialectDescriptorIndex.DialectInfo>> resources) {
                 // TODO TldProcessorFactory.getFactory(context).processResources(resources);
@@ -97,13 +99,16 @@ public class DialectDescriptorIndex extends XmlIndex<DialectDescriptorIndex.Dial
         });
     }
 
+    @Override
     @NotNull
     public ID<String, DialectInfo> getName() {
         return NAME;
     }
 
+    @Override
     public FileBasedIndex.InputFilter getInputFilter() {
         return new FileBasedIndex.InputFilter() {
+            @Override
             public boolean acceptInput(VirtualFile file) {
                 if (XmlFileType.INSTANCE != file.getFileType()) return false;
                 String name = file.getName();
@@ -112,9 +117,11 @@ public class DialectDescriptorIndex extends XmlIndex<DialectDescriptorIndex.Dial
         };
     }
 
+    @Override
     @NotNull
     public DataIndexer<String, DialectInfo, FileContent> getIndexer() {
         return new DataIndexer<String, DialectInfo, FileContent>() {
+            @Override
             @NotNull
             public Map<String, DialectInfo> map(FileContent inputData) {
                 final DialectUriXmlBuilder builder = new DialectUriXmlBuilder();
@@ -130,18 +137,22 @@ public class DialectDescriptorIndex extends XmlIndex<DialectDescriptorIndex.Dial
         };
     }
 
+    @Override
     public DataExternalizer<DialectInfo> getValueExternalizer() {
         return new DataExternalizer<DialectInfo>() {
+            @Override
             public void save(DataOutput out, DialectDescriptorIndex.DialectInfo value) throws IOException {
                 out.writeUTF(value.prefix);
             }
 
+            @Override
             public DialectDescriptorIndex.DialectInfo read(DataInput in) throws IOException {
                 return new DialectDescriptorIndex.DialectInfo(in.readUTF());
             }
         };
     }
 
+    @Override
     public int getVersion() {
         return INDEX_VERSION;
     }
